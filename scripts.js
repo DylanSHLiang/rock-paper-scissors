3fa094c4f040fcf8b26347c0b0c7a1045679a7fd
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2023-08-28 00:34:19 +1000

    Added functions to finish one game

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const outcomes = document.querySelectorAll("div.user, div.comp");
+const scores = document.querySelectorAll("p.score");
 const message = document.querySelector("h1");
 const options = document.querySelectorAll("div.choice");
+const reset = document.querySelector("button");
 
 
 const choices = ["Rock", "Paper", "Scissors"];
@@ -28,10 +30,35 @@ function playRound(player, computer) {
         (computer > player && computer - player == 2)
     ) {
         result = `You Win! ${choices[player]} beats ${choices[computer]}`;
+        scores[0].innerText++;
     } else {
         result = `You Lose! ${choices[computer]} beats ${choices[player]}`;
+        scores[1].innerText++;
+    }
+    if (scores[0].innerText == 5) {
+        options.forEach(element => {
+            element.style["pointer-events"] = "none"; 
+        })
+        return "You Win the Game!";
+    }
+    if (scores[1].innerText == 5) {
+        options.forEach(element => {
+            element.style["pointer-events"] = "none"; 
+        })
+        return "You Lose the Game!";
     }
     return result
+}
+
+function displayChoices(choice, index) {
+    if (choice == 0) {
+        outcomes[index].querySelector("img").src = "./images/rock.png";
+    } else if (choice == 1) {
+        outcomes[index].querySelector("img").src = "./images/paper.png";
+    } else {
+        outcomes[index].querySelector("img").src = "./images/scissors.png";
+    }
+    
 }
 
 function game(input) {
@@ -42,7 +69,11 @@ function game(input) {
     } else {
         element = input
     }
-    console.log(playRound(getPlayerChoice(element.id), getComputerChoice()));
+    let player = getPlayerChoice(element.id);
+    let computer = getComputerChoice();
+    displayChoices(player, 0);
+    displayChoices(computer, 1);
+    message.innerText = playRound(player, computer);
 }
 
 options.forEach(element => {
@@ -51,10 +82,6 @@ options.forEach(element => {
     });
 });
 
-/*
-item = element.querySelector("img");
-console.log(item);
-if (item.id.includes("rock")) {
-    console.log(item.id);
-}
-*/
+
+
+
